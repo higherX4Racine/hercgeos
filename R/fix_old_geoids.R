073c5a3c7e18ddd156a2a32b722b5cea734f6752
Ben Taft <ben.taft@career2cradle.org>
2023-07-02 13:19:56 -0400

    begun

---
+++ R/fix_old_geoids.R
@@ -0,0 +1,16 @@
+#' Replace ID fields with "GEOID"
+#'
+#' Older (pre-2010) shapefiles have geography-specific names for their GEOID fields.
+#' For example, a county subdivision shapefile would have a field named "COSBIDFP."
+#' This function renames any field that includes "IDFP" to "GEOID."
+#'
+#' @param .features a simple features object
+#'
+#' @return the same object, but with one column possibly renamed.
+#' @export
+#'
+fix_old_geoids <- function(.features){
+    dplyr::rename_with(.features,
+                       .fn = ~ "GEOID",
+                       .cols = tidyselect::contains("IDFP"))
+}
